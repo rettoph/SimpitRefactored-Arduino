7b3282459557e0816319f5ca5da0eccc159caa8e
rettoph <anthony@turner.ec>
2024-07-05 11:51:47 -0600

    Minor script bug fixes

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,7 +2,7 @@ set KerbalSimpitRevampedArduinoTargetPath="%USERPROFILE%\Documents\Arduino\libra
 set FunctionObjectsTargetPath="%USERPROFILE%\Documents\Arduino\libraries\function_objects"
 
 if exist %KerbalSimpitRevampedArduinoTargetPath% rmdir %KerbalSimpitRevampedArduinoTargetPath%
-mklink /J %KerbalSimpitRevampedArduinoTargetPath% %~dp0\..
+mklink /J %KerbalSimpitRevampedArduinoTargetPath% "%~dp0\.."
 
 if exist %FunctionObjectsTargetPath% rmdir %FunctionObjectsTargetPath%
-mklink /J %FunctionObjectsTargetPath% %~dp0\..\libraries\function_objects\include
+mklink /J %FunctionObjectsTargetPath% "%~dp0\..\libraries\function_objects\include"
