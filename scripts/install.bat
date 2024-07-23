0058a91b847ffdc7a6ba24293430477780c980cf
rettoph <anthony@turner.ec>
2024-07-23 11:03:04 -0600

    Improve install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,8 +1,37 @@
-set KerbalSimpitRevampedArduinoTargetPath="%USERPROFILE%\Documents\Arduino\libraries\KerbalSimpitRevamped-Arduino"
-set FunctionObjectsTargetPath="%USERPROFILE%\Documents\Arduino\libraries\function_objects"
+set ArduinoLibraryPath=%USERPROFILE%\Documents\Arduino\libraries
+set KerbalSimpitRevampedArduinoTargetPath="%ArduinoLibraryPath%\KerbalSimpitRevamped-Arduino"
+set FunctionObjectsTargetPath="%ArduinoLibraryPath%\function_objects"
 
-if exist %KerbalSimpitRevampedArduinoTargetPath% rmdir %KerbalSimpitRevampedArduinoTargetPath%
-mklink /J %KerbalSimpitRevampedArduinoTargetPath% "%~dp0\.."
+CALL :NORMALIZEPATH "%~dp0\.."
+SET KerbalSimpitRevampedArduinoSourcePath=%RETVAL%
 
-if exist %FunctionObjectsTargetPath% rmdir %FunctionObjectsTargetPath%
-mklink /J %FunctionObjectsTargetPath% "%~dp0\..\libraries\function_objects\include"
+CALL :NORMALIZEPATH "%~dp0\..\libraries\function_objects\include"
+SET FunctionObjectsSourcePath=%RETVAL%
+
+if not exist %KerbalSimpitRevampedArduinoTargetPath% (
+    echo Creating Arduino Library Directory at %ArduinoLibraryPath%
+    mkdir %ArduinoLibraryPath%
+)
+
+if exist %KerbalSimpitRevampedArduinoTargetPath% (
+    echo Removing existing KerbalSimpitRevampedArduino instace at %KerbalSimpitRevampedArduinoTargetPath%
+    rmdir %KerbalSimpitRevampedArduinoTargetPath%
+)
+
+mklink /J %KerbalSimpitRevampedArduinoTargetPath% %KerbalSimpitRevampedArduinoSourcePath%
+
+if exist %FunctionObjectsTargetPath% (
+    echo Removing existing function_objects instace at %FunctionObjectsTargetPath%
+    rmdir %FunctionObjectsTargetPath%
+)
+
+mklink /J %FunctionObjectsTargetPath% %FunctionObjectsSourcePath%
+
+echo Done!
+
+:: ========== FUNCTIONS ==========
+EXIT /B
+
+:NORMALIZEPATH
+  SET RETVAL=%~f1
+  EXIT /B
